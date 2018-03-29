15e32c4e22cb1ab222e0f98326f140e5b0c91aa7
Andrew Bartson <andrewbartson@Andrews-MacBook-Pro.local>
2018-03-29 13:54:01 -0700

    Add american_state_input.js - seed input modifications

--- seed_data/american_state_input.js
+++ seed_data/american_state_input.js
@@ -3,306 +3,312 @@ module.exports = [
     "state_name": "Alabama",
     "electoral_votes": 9,
     "state_code": "AL",
+    "total_votes": 0,
     "color_group": 1  
   },
   {
     "state_name": "Alaska",
     "electoral_votes": 3,
     "state_code": "AK", 
+    "total_votes": 0, 
     "color_group": 3  
   },
   {
     "state_name": "Arizona",
     "electoral_votes": 11,
     "state_code": "AZ", 
+    "total_votes": 0, 
     "color_group": 4  
   },
   {
     "state_name": "Arkansas",
     "electoral_votes": 6,
     "state_code": "AR", 
+    "total_votes": 0, 
     "color_group": 1  
   },
   {
     "state_name": "California",
     "electoral_votes": 55,
     "state_code": "CA", 
+    "total_votes": 0, 
     "color_group": 8  
   },
   {
     "state_name": "Colorado",
     "electoral_votes": 9,
     "state_code": "CO", 
+    "total_votes": 0, 
     "color_group": 5  
   },
   {
     "state_name": "Connecticut",
     "electoral_votes": 7,
     "state_code": "CT", 
-    "color_group": 6  
+    "total_votes": 0, "color_group": 6  
   },
   {
     "state_name": "Delaware",
     "electoral_votes": 3,
     "state_code": "DE", 
-    "color_group": 6  
+    "total_votes": 0, "color_group": 6  
   },
   {
     "state_name": "District of Columbia",
     "electoral_votes": 3,
     "state_code": "DC", 
-    "color_group": 8  
+    "total_votes": 0, "color_group": 8  
   },
   {
     "state_name": "Florida",
     "electoral_votes": 29,
     "state_code": "FL", 
-    "color_group": 4  
+    "total_votes": 0, "color_group": 4  
   },
   {
     "state_name": "Georgia",
     "electoral_votes": 16,
     "state_code": "GA", 
-    "color_group": 3  
+    "total_votes": 0, "color_group": 3  
   },
   {
     "state_name": "Hawaii",
     "electoral_votes": 4,
     "state_code": "HI", 
-    "color_group": 8  
+    "total_votes": 0, "color_group": 8  
   },
   {
     "state_name": "Idaho",
     "electoral_votes": 4,
     "state_code": "ID", 
-    "color_group": 1 
+    "total_votes": 0, "color_group": 1 
   },
   {
     "state_name": "Illinois",
     "electoral_votes": 20,
     "state_code": "IL", 
-    "color_group": 7  
+    "total_votes": 0, "color_group": 7  
   },
   {
     "state_name": "Indiana",
     "electoral_votes": 11,
     "state_code": "IN", 
-    "color_group": 2  
+    "total_votes": 0, "color_group": 2  
   },
   {
     "state_name": "Iowa",
     "electoral_votes": 6,
     "state_code": "IA", 
-    "color_group": 3  
+    "total_votes": 0, "color_group": 3  
   },
   {
     "state_name": "Kansas",
     "electoral_votes": 6,
     "state_code": "KS", 
-    "color_group": 2  
+    "total_votes": 0, "color_group": 2  
   },
   {
     "state_name": "Kentucky",
     "electoral_votes": 8,
     "state_code": "KY", 
-    "color_group": 1  
+    "total_votes": 0, "color_group": 1  
   },
   {
     "state_name": "Louisiana",
     "electoral_votes": 8,
     "state_code": "LA", 
-    "color_group": 2  
+    "total_votes": 0, "color_group": 2  
   },
   {
     "state_name": "Maine",
     "electoral_votes": 4,
     "state_code": "ME", 
-    "color_group": 5  
+    "total_votes": 0, "color_group": 5  
   },
   {
     "state_name": "Maryland",
     "electoral_votes": 10,
     "state_code": "MD",
-    "color_group": 8  
+    "total_votes": 0, "color_group": 8  
   },
   {
     "state_name": "Massachusetts",
     "electoral_votes": 11,
     "state_code": "MA", 
-    "color_group": 8  
+    "total_votes": 0, "color_group": 8  
   },
   {
     "state_name": "Michigan",
     "electoral_votes": 16,
     "state_code": "MI", 
-    "color_group": 4  
+    "total_votes": 0, "color_group": 4  
   },
   {
     "state_name": "Minnesota",
     "electoral_votes": 10,
     "state_code": "MN", 
-    "color_group": 5  
+    "total_votes": 0, "color_group": 5  
   },
   {
     "state_name": "Mississippi",
     "electoral_votes": 6,
     "state_code": "MS", 
-    "color_group": 2  
+    "total_votes": 0, "color_group": 2  
   },
   {
     "state_name": "Missouri",
     "electoral_votes": 10,
     "state_code": "MO", 
-    "color_group": 2  
+    "total_votes": 0, "color_group": 2  
   },
   {
     "state_name": "Montana",
     "electoral_votes": 3,
     "state_code": "MT", 
-    "color_group": 2  
+    "total_votes": 0, "color_group": 2  
   },
   {
     "state_name": "Nebraska",
     "electoral_votes": 5,
     "state_code": "NE", 
-    "color_group": 1  
+    "total_votes": 0, "color_group": 1  
   },
   {
     "state_name": "Nevada",
     "electoral_votes": 6,
     "state_code": "NV", 
-    "color_group": 5  
+    "total_votes": 0, "color_group": 5  
   },
   {
     "state_name": "New Hampshire",
     "electoral_votes": 4,
     "state_code": "NH", 
-    "color_group": 5  
+    "total_votes": 0, "color_group": 5  
   },
   {
     "state_name": "New Jersey",
     "electoral_votes": 14,
     "state_code": "NJ", 
-    "color_group": 6  
+    "total_votes": 0, "color_group": 6  
   },
   {
     "state_name": "New Mexico",
     "electoral_votes": 5,
     "state_code": "NM", 
-    "color_group": 6  
+    "total_votes": 0, "color_group": 6  
   },
   {
     "state_name": "New York",
     "electoral_votes": 29,
     "state_code": "NY", 
-    "color_group": 7  
+    "total_votes": 0, "color_group": 7  
   },
   {
     "state_name": "North Carolina",
     "electoral_votes": 15,
     "state_code": "NC", 
-    "color_group": 4  
+    "total_votes": 0, "color_group": 4  
   },
   {
     "state_name": "North Dakota",
     "electoral_votes": 3,
     "state_code": "ND", 
-    "color_group": 1  
+    "total_votes": 0, "color_group": 1  
   },
   {
     "state_name": "Ohio",
     "electoral_votes": 18,
     "state_code": "OH", 
-    "color_group": 3  
+    "total_votes": 0, "color_group": 3  
   },
   {
     "state_name": "Oklahoma",
     "electoral_votes": 7,
     "state_code": "OK", 
-    "color_group": 1  
+    "total_votes": 0, "color_group": 1  
   },
   {
     "state_name": "Oregon",
     "electoral_votes": 7,
     "state_code": "OR", 
-    "color_group": 6  
+    "total_votes": 0, "color_group": 6  
   },
   {
     "state_name": "Pennsylvania",
     "electoral_votes": 20,
     "state_code": "PA", 
-    "color_group": 4  
+    "total_votes": 0, "color_group": 4  
   },
   {
     "state_name": "Rhode Island",
     "electoral_votes": 4,
     "state_code": "RI", 
-    "color_group": 7  
+    "total_votes": 0, "color_group": 7  
   },
   {
     "state_name": "South Carolina",
     "electoral_votes": 9,
     "state_code": "SC", 
-    "color_group": 3  
+    "total_votes": 0, "color_group": 3  
   },
   {
     "state_name": "South Dakota",
     "electoral_votes": 3,
     "state_code": "SD", 
-    "color_group": 1  
+    "total_votes": 0, "color_group": 1  
   },
   {
     "state_name": "Tennessee",
     "electoral_votes": 11,
     "state_code": "TN", 
-    "color_group": 1  
+    "total_votes": 0, "color_group": 1  
   },
   {
     "state_name": "Texas",
     "electoral_votes": 38,
     "state_code": "TX",
-    "color_group": 3  
+    "total_votes": 0, "color_group": 3  
   },
   {
     "state_name": "Utah",
     "electoral_votes": 6,
     "state_code": "UT", 
-    "color_group": 2  
+    "total_votes": 0, "color_group": 2  
   },
   {
     "state_name": "Vermont",
     "electoral_votes": 3,
     "state_code": "VT", 
-    "color_group": 8  
+    "total_votes": 0, "color_group": 8  
   },
   {
     "state_name": "Virginia",
     "electoral_votes": 13,
     "state_code": "VA", 
-    "color_group": 6  
+    "total_votes": 0, "color_group": 6  
   },
   {
     "state_name": "Washington",
     "electoral_votes": 12,
     "state_code": "WA", 
-    "color_group": 7  
+    "total_votes": 0, "color_group": 7  
   },
   {
     "state_name": "West Virginia",
     "electoral_votes": 5,
     "state_code": "WV", 
-    "color_group": 1  
+    "total_votes": 0, "color_group": 1  
   },
   {
     "state_name": "Wisconsin",
     "electoral_votes": 10,
     "state_code": "WI",
-    "color_group": 4
+    "total_votes": 0, "color_group": 4
   },
   {
     "state_name": "Wyoming",
     "electoral_votes": 3,
     "state_code": "WY",
-    "color_group": 1
+    "total_votes": 0, "color_group": 1
   }
 ]
