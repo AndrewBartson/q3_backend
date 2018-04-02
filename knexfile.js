b479e450e0dbb413c21953e9424a4e59ea616d0b
Andrew Bartson <andrewbartson@Andrews-MacBook-Pro.local>
2018-04-02 11:55:37 -0700

    Fix one parameter in knexfile

--- knexfile.js
+++ knexfile.js
@@ -16,7 +16,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env. ,
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: __dirname + '/migrations',
     },
